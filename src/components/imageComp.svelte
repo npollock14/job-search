3cc2afa8d5d22141eec7dbb230dc317620756cdb
npollock14 <ndpollock@wpi.edu>
2022-09-04 18:25:13 -0400

    error handling for images

--- src/components/imageComp.svelte
+++ src/components/imageComp.svelte
@@ -1,10 +1,13 @@
 <script>
 	export let row;
 	export let col;
+
+	const fallback = 'https://via.placeholder.com/150';
+	const handleError = (ev) => (ev.target.src = fallback);
 </script>
 
 <div>
-	<img src={row.icon} alt={row.icon} />
+	<img src={row.icon} alt={row.icon} on:error={handleError} />
 </div>
 
 <style>
